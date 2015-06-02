LiquidSmooth-Old/android_packages_apps_Settings

Blacklist (2/8)
Add back blacklist settings.

Conflicts:
	AndroidManifest.xml
	res/values/cm_arrays.xml
	res/values/cm_strings.xml

Add missing blacklist xxhdpi drawables.

Settings: Alert user when they attempt to add an invalid number to BL.

  - BlacklistProvider will reject numbers that aren't valid, silently. So instead of
  resuming to a blank screen, we should alert the user that the number they attempted to
  add is invalid by utilizing BlacklistUtils#addOrUpdate method.

Conflicts:
	res/values/cm_strings.xml

Settings : Launch only contacts picker for blacklist entry

Blacklist: Fix launching blacklist settings

Settings: materialize Blacklist settings

* Add a FAB
* Add a (generic system) enable/disable switch bar

Signed-off-by: Roman Birg <roman@cyngn.com>

Conflicts:
	src/com/android/settings/cyanogenmod/BaseSystemSettingSwitchBar.java
	src/com/android/settings/profiles/ProfileEnabler.java
	src/com/android/settings/profiles/ProfilesList.java
	src/com/android/settings/profiles/ProfilesSettings.java
	src/com/android/settings/slim/BaseSystemSettingSwitchBar.java

Conflicts:
	res/values/slim_colors.xml
	res/values/styles.xml

Settings: add a way to open blacklist settings externally

Opening the activity doesn't theme the switch bar - an alias works.

Signed-off-by: Roman Birg <roman@cyngn.com>

blacklist: make it possible to unblacklist phone/messages via checkboxes

Settings: fix blacklist settings crash

On a device rotate, the MultiSelectListPreference would try and restore
its state since the preferences aren't persisted. Clear the persisted
flag so the MultiSelectPreference doesn't try and restore the states, as
they get populated in onResume().

Signed-off-by: Roman Birg <roman@cyngn.com>

Fix up some blacklist things

Signed-off-by: RobbieL811 <RobbieLancaster811@gmail.com>

Fix blacklist setting FC

Conflicts:
	AndroidManifest.xml
	res/values/slim_arrays.xml
	src/com/android/settings/Settings.java
	src/com/android/settings/SettingsActivity.java

Ch

src/com/android/settings/SecuritySettings.java
@@ -48,12 +48,14 @@ import android.telephony.SubscriptionInfo;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.android.internal.telephony.util.BlacklistUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.TrustAgentUtils.TrustAgentComponentInfo;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Index;
 import com.android.settings.search.Indexable;
 import com.android.settings.search.SearchIndexableRaw;
+import com.android.settings.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -114,6 +116,11 @@ public class SecuritySettings extends SettingsPreferenceFragment
     // Only allow one trust agent on the platform.
     private static final boolean ONLY_ONE_TRUST_AGENT = true;
 
+    // CyanogenMod Additions
+    private static final String KEY_APP_SECURITY_CATEGORY = "app_security";
+    private static final String KEY_BLACKLIST = "blacklist";
+
+    private PackageManager mPM;
     private DevicePolicyManager mDPM;
     private SubscriptionManager mSubscriptionManager;
 
@@ -140,6 +147,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private boolean mIsPrimary;
 
     private Intent mTrustAgentClickIntent;
+    // CyanogenMod Additions
+    private PreferenceScreen mBlacklist;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -161,6 +170,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
     private static int getResIdForLockUnlockScreen(Context context,
             LockPatternUtils lockPatternUtils) {
+        // Add options for lock/unlock screen
         int resid = 0;
         if (!lockPatternUtils.isSecure()) {
             // if there are multiple users, disable "None" setting
@@ -211,6 +221,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
         }
         addPreferencesFromResource(R.xml.security_settings);
         root = getPreferenceScreen();
+        // Add package manager to check if features are available
+        PackageManager pm = getActivity().getPackageManager();
 
         // Add options for device encryption
         mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
@@ -411,6 +423,18 @@ public class SecuritySettings extends SettingsPreferenceFragment
                 manageAgents.setSummary(R.string.disabled_because_no_backup_security);
             }
 
+        }
+
+        // App security settings
+        addPreferencesFromResource(R.xml.security_settings_app_slim);
+        mBlacklist = (PreferenceScreen) root.findPreference(KEY_BLACKLIST);
+
+        // Determine options based on device telephony support
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+            // No telephony, remove dependent options
+            PreferenceGroup appCategory = (PreferenceGroup)
+                    root.findPreference(KEY_APP_SECURITY_CATEGORY);
+            appCategory.removePreference(mBlacklist);
         }
 
         // The above preferences come and go based on security state, so we need to update
@@ -637,6 +661,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
         if (mResetCredentials != null) {
             mResetCredentials.setEnabled(!mKeyStore.isEmpty());
         }
+
+        updateBlacklistSummary();
     }
 
     @Override
@@ -933,4 +959,13 @@ public class SecuritySettings extends SettingsPreferenceFragment
         }
     }
 
+    private void updateBlacklistSummary() {
+        if (mBlacklist != null) {
+            if (BlacklistUtils.isBlacklistEnabled(getActivity())) {
+                mBlacklist.setSummary(R.string.blacklist_summary);
+            } else {
+                mBlacklist.setSummary(R.string.blacklist_summary_disabled);
+            }
+        }
+    }
 }

InfinitiveOS/packages_apps_settings

	res/values/dimens.xml
	res/values/slim_strings.xml
	res/xml/security_settings_app_slim.xml

src/com/android/settings/SecuritySettings.java
@@ -51,12 +51,14 @@ import android.telephony.SubscriptionInfo;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.android.internal.telephony.util.BlacklistUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.TrustAgentUtils.TrustAgentComponentInfo;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Index;
 import com.android.settings.search.Indexable;
 import com.android.settings.search.SearchIndexableRaw;
+import com.android.settings.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -115,6 +117,11 @@ public class SecuritySettings extends SettingsPreferenceFragment
     // Only allow one trust agent on the platform.
     private static final boolean ONLY_ONE_TRUST_AGENT = true;
 
+    // CyanogenMod Additions
+    private static final String KEY_APP_SECURITY_CATEGORY = "app_security";
+    private static final String KEY_BLACKLIST = "blacklist";
+
+    private PackageManager mPM;
     private DevicePolicyManager mDPM;
     private SubscriptionManager mSubscriptionManager;
 
@@ -137,6 +144,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private boolean mIsPrimary;
 
     private Intent mTrustAgentClickIntent;
+    // CyanogenMod Additions
+    private PreferenceScreen mBlacklist;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -158,6 +167,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
     private static int getResIdForLockUnlockScreen(Context context,
             LockPatternUtils lockPatternUtils) {
+        // Add options for lock/unlock screen
         int resid = 0;
         if (!lockPatternUtils.isSecure()) {
             // if there are multiple users, disable "None" setting
@@ -205,6 +215,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
         }
         addPreferencesFromResource(R.xml.security_settings);
         root = getPreferenceScreen();
+        // Add package manager to check if features are available
+        PackageManager pm = getActivity().getPackageManager();
 
         // Add options for lock/unlock screen
         final int resid = getResIdForLockUnlockScreen(getActivity(), mLockPatternUtils);
@@ -400,6 +412,18 @@ public class SecuritySettings extends SettingsPreferenceFragment
                 manageAgents.setEnabled(false);
                 manageAgents.setSummary(R.string.disabled_because_no_backup_security);
             }
+        }
+
+        // App security settings
+        addPreferencesFromResource(R.xml.security_settings_app_io);
+        mBlacklist = (PreferenceScreen) root.findPreference(KEY_BLACKLIST);
+
+        // Determine options based on device telephony support
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+            // No telephony, remove dependent options
+            PreferenceGroup appCategory = (PreferenceGroup)
+                    root.findPreference(KEY_APP_SECURITY_CATEGORY);
+            appCategory.removePreference(mBlacklist);
         }
 
         // The above preferences come and go based on security state, so we need to update
@@ -626,6 +650,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
         if (mResetCredentials != null) {
             mResetCredentials.setEnabled(!mKeyStore.isEmpty());
         }
+
+        updateBlacklistSummary();
     }
 
     @Override
@@ -905,4 +931,13 @@ public class SecuritySettings extends SettingsPreferenceFragment
         }
     }
 
+    private void updateBlacklistSummary() {
+        if (mBlacklist != null) {
+            if (BlacklistUtils.isBlacklistEnabled(getActivity())) {
+                mBlacklist.setSummary(R.string.blacklist_summary);
+            } else {
+                mBlacklist.setSummary(R.string.blacklist_summary_disabled);
+            }
+        }
+    }
 }